0def5c14e1b241272323d70d0fb8dfd0c5b775ac
Oleg Kozlov <h1karo@outlook.com>
2020-04-28 23:35:16 +0300

    Configure testing via phpunit

---
+++ tests/Kernel.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * This file is a part of Relations Messenger Message Standard Bundle.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/message-bundle
+ * @link      https://dev.relmsg.ru/packages/message-bundle
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/message-bundle
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Bundle\MessageBundle\Tests;
+
+use Exception;
+use Psr\Log\NullLogger;
+use RM\Bundle\MessageBundle\MessageBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+/**
+ * Class Kernel
+ *
+ * @package RM\Bundle\MessageBundle\Tests
+ * @author  h1karo <h1karo@outlook.com>
+ */
+class Kernel extends BaseKernel
+{
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    private ?string $testCase;
+
+    public function __construct(string $environment, bool $debug, string $testCase = null)
+    {
+        parent::__construct($environment, $debug);
+
+        $this->testCase = $testCase;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new MessageBundle(),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/MessageBundle/cache';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/MessageBundle/log';
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $confDir = __DIR__ . '/config';
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+
+        $testCaseDir = implode('/', [$confDir, $this->testCase]);
+        if ($this->testCase && file_exists($testCaseDir) && is_dir($testCaseDir)) {
+            $loader->load($testCaseDir . '/*' . self::CONFIG_EXTS, 'glob');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->register('logger', NullLogger::class);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is a part of Relations Messenger Message Standard Bundle.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/message-bundle
+ * @link      https://dev.relmsg.ru/packages/message-bundle
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/message-bundle
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Bundle\MessageBundle\Tests;
+
+use ReflectionClass;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Class TestCase
+ *
+ * @package RM\Bundle\MessageBundle\Tests
+ * @author  h1karo <h1karo@outlook.com>
+ */
+abstract class TestCase extends WebTestCase
+{
+    protected function setUp(): void
+    {
+        $fs = new Filesystem();
+        $fs->remove(sys_get_temp_dir() . '/MessageBundle/');
+    }
+
+    protected static function createKernel(array $options = []): KernelInterface
+    {
+        if (null === static::$class) {
+            static::$class = static::getKernelClass();
+        }
+
+        if (isset($options['environment'])) {
+            $env = $options['environment'];
+        } elseif (isset($_ENV['APP_ENV'])) {
+            $env = $_ENV['APP_ENV'];
+        } elseif (isset($_SERVER['APP_ENV'])) {
+            $env = $_SERVER['APP_ENV'];
+        } else {
+            $env = 'test';
+        }
+
+        if (isset($options['debug'])) {
+            $debug = $options['debug'];
+        } elseif (isset($_ENV['APP_DEBUG'])) {
+            $debug = $_ENV['APP_DEBUG'];
+        } elseif (isset($_SERVER['APP_DEBUG'])) {
+            $debug = $_SERVER['APP_DEBUG'];
+        } else {
+            $debug = true;
+        }
+
+        $reflect = new ReflectionClass(static::class);
+        $testCase = str_replace('Test', '', $reflect->getShortName());
+
+        return new static::$class($env, $debug, $testCase);
+    }
+}
