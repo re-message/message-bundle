331698b3479c26cb6d0bdc1331276c1f21cc07dc
Oleg Kozlov <h1karo@remessage.ru>
2025-06-14 12:53:42 +0300

    Update copyright in file header

--- config/formatters.php
+++ config/formatters.php
@@ -1,17 +1,16 @@
 <?php
+
 /*
- * This file is a part of Message Standard Bundle.
- * This package is a part of Re Message.
+ * This file is part of Message Standard Bundle.
  *
- * @link      https://github.com/re-message/message-bundle
- * @link      https://dev.remessage.ru/packages/message-bundle
- * @copyright Copyright (c) 2018-2023 Re Message
- * @author    Oleg Kozlov <h1karo@remessage.ru>
- * @license   Apache License 2.0
- * @license   https://legal.remessage.ru/licenses/message-bundle
+ * (c) 2018-present Re Message
+ *     Oleg Kozlov <h1karo@remessage.ru>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * https://dev.remessage.ru/packages/message-bundle
+ * https://github.com/re-message/message-bundle
  */
 
 use RM\Standard\Message\Format\JsonMessageFormatter;
